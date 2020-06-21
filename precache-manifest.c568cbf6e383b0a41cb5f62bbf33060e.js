4cfa1c3316166cf08c211328c776f4b26ae748a3
unknown <martinfzb@sina.com>
2020-06-21 11:01:54 +0800

    Updates

---
+++ precache-manifest.c568cbf6e383b0a41cb5f62bbf33060e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55af1178dcaa863946e82e58daad338",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "8cf6b83617e71b9a3965",
+    "url": "/deploy-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad0905faaaf4759825ab",
+    "url": "/deploy-react/static/js/2.ea08002d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.ea08002d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf6b83617e71b9a3965",
+    "url": "/deploy-react/static/js/main.2126470a.chunk.js"
+  },
+  {
+    "revision": "d4aa56b5df3f830504c6",
+    "url": "/deploy-react/static/js/runtime-main.bf59351e.js"
+  }
+]);
